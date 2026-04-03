c20e36269b5bdd751349c34ae8ba67591c740183
ASSGA <deafdonascimento@gmail.com>
2026-04-03 17:56:14 +0000

    fix create table vercel

--- associacao/views.py
+++ associacao/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from .models import Socio
+from django.db import connection
+
 
 def home(request):
-    socios = Socio.objects.all()
-    return render(request, 'home.html', {'socios': socios})
+    try:
+        socios = Socio.objects.all()
+    except:
+        with connection.schema_editor() as schema:
+            schema.create_model(Socio)
+        socios = Socio.objects.all()
+
+    return render(request, 'associacao/home.html', {'socios': socios})
