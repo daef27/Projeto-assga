a86275d1bdf53acce5e6d970ec62f0447937b878
ASSGA <deafdonascimento@gmail.com>
2026-04-03 18:11:02 +0000

    auto migrate vercel

--- core/wsgi.py
+++ core/wsgi.py
@@ -14,3 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_wsgi_application()
+
+# MIGRATE AUTOMATICO
+from django.core.management import call_command
+
+try:
+    call_command('migrate', interactive=False)
+except:
+    pass
