879f7a777b8c9a0e13233c9b2101f6774490a0f2
ASSGA <deafdonascimento@gmail.com>
2026-04-07 04:31:26 +0000

    Fix static files

--- core/settings.py
+++ core/settings.py
@@ -146,14 +146,13 @@ USE_TZ = True
 # ========================
 # STATIC FILES
 # ========================
-
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
