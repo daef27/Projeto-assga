91cdcf51ed2c45089c10913fd6d35491b5fb1fa0
ASSGA <deafdonascimento@gmail.com>
2026-05-15 18:36:50 -0300

    Add env var check to create_superuser script

--- create_superuser.py
+++ create_superuser.py
@@ -8,12 +8,15 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin')
-email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
 
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, password=password, email=email)
-    print(f'Superusuário "{username}" criado com sucesso!')
+if not username or not password:
+    print('Variáveis de ambiente DJANGO_SUPERUSER_USERNAME e DJANGO_SUPERUSER_PASSWORD não definidas. Pulando criação de superusuário.')
 else:
-    print(f'Superusuário "{username}" já existe.')
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, password=password, email=email or 'admin@example.com')
+        print(f'Superusuário "{username}" criado com sucesso!')
+    else:
+        print(f'Superusuário "{username}" já existe.')
