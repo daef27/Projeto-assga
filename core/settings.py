b9bbadb6705c9d44205340379845054e2a23884a
ASSGA <deafdonascimento@gmail.com>
2026-05-16 22:44:42 -0300

    Remove SQLite fallback to force real error

--- core/settings.py
+++ core/settings.py
@@ -124,14 +124,11 @@ DATABASE_URL = os.environ.get("DATABASE_URL", "").strip()
 
 
 if DATABASE_URL:
-    try:
-        DATABASES = {
-            "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
-        }
-    except Exception:
-        DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}}
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
+    }
 else:
-    DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}}
+    raise ImproperlyConfigured("DATABASE_URL environment variable not set")
 
 
 
