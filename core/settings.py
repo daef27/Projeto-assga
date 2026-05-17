84296393666776358616999d53707da5d618d35d
ASSGA <deafdonascimento@gmail.com>
2026-05-17 00:03:49 -0300

    Add browser preview to CSRF_TRUSTED_ORIGINS

--- core/settings.py
+++ core/settings.py
@@ -26,15 +26,11 @@ ALLOWED_HOSTS = ["*"]
 
 
 CSRF_TRUSTED_ORIGINS = [
-
     "http://127.0.0.1:8000",
-
     "http://localhost:8000",
-
+    "http://127.0.0.1:60726",
     "https://projeto-alpha-weld.vercel.app",
-
     "https://*.vercel.app",
-
 ]
 
 
